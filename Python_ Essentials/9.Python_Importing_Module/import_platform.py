2ac71d2028500f1bd8acfab97609b95cf2e9918d
sethaddie <abayoseth@gmail.com>
2023-01-31 15:13:41 +0300

    progress update sync

--- Python_ Essentials/9.Python_Importing_Module/import_platform.py
+++ Python_ Essentials/9.Python_Importing_Module/import_platform.py
@@ -1,11 +1,16 @@
-from platform import platform, machine, processor, system
+from platform import platform, machine, processor, system, version, python_implementation, python_version_tuple
 
 print(platform())
-print(platform(1))
-print(platform(0, 1))
+#print(platform(1))
+#print(platform(0, 1))
 print(platform(aliased = True, terse= False))
 
 
 print(machine())
 print(processor())
 print(system())
+print(version())
+print(python_implementation())
+
+for atr in python_version_tuple():
+    print(atr)
